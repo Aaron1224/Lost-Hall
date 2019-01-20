233ffda24231a9ea38dc6fa4546af38a9b2b0e6b
Marco Brunetti <marco19-96@hotmail.com>
2019-01-20 01:01:29 -0300

    Created move methods, floor collider
    - Added floor collider
    - Created moving methods for player
    - Created a player holder

---
+++ Lost Hall/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Animator walkanimator;
+    public GameObject player;
+    public Sprite walk_0;
+
+    void Start()
+    {
+        player.GetComponent<SpriteRenderer>().sprite = walk_0;
+
+        walkanimator = player.GetComponent<Animator>();
+        walkanimator.enabled = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            walkanimator.enabled = true;
+            player.GetComponent<SpriteRenderer>().flipX = true;
+            transform.position = new Vector3(transform.position.x - 0.04f, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            walkanimator.enabled = true;
+            player.GetComponent<SpriteRenderer>().flipX = false;
+            transform.position = new Vector3(transform.position.x + 0.04f, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            walkanimator.enabled = false;
+            player.GetComponent<SpriteRenderer>().sprite = walk_0;
+        }
+    }
+}
